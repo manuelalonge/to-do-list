babec29332584a3b4e2f33de9ae3933a0a0884b4
Manuel Alonge <manuel.alonge@gmail.com>
2020-02-20 15:17:03 +0100

    20022020 15:17

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// dichiarazione variabili globali
 let itemsList;
 let clearList;
 
-// All'init dell'applicazione prendo le note dal local storage
+
 let storedNotes = Object.entries(window.localStorage).filter(entity => entity[0].includes('notes-'));
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,36 +12,35 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('storedNotes: ', storedNotes);
     storedNotes.forEach(note => addItemToDom(note[1], itemsList, note[0]));
 
-    // aggiunta di un item tramite click del bottone add
-    addButton.addEventListener('click', (event) => {
 
-        const noteId = 'notes-' + Date.now(); // genero ID
-        var text = document.querySelector('#inputField').value; // testo della nota
-        addItemToDom(text, itemsList, noteId); // aggiungo un elemento della lista al DOM
-        window.localStorage.setItem(noteId, text); // setto al local storage la nuova nota
-        storedNotes.push([noteId, text]); // salvo in memoria nel mio array la nuova nota
+    addButton.addEventListener('click', () => {
+
+        const noteId = 'notes-' + Date.now();
+        var text = document.querySelector('#inputField').value;
+        addItemToDom(text, itemsList, noteId);
+        window.localStorage.setItem(noteId, text);
+        storedNotes.push([noteId, text]);
         text = null;
 
     });
 
-    // rimozione di tutti gli items tramite il bottone 'CLEAR LIST'
+
     clearList.addEventListener('click', (e) => {
-        // ciclo le note salvate in memoria nel mio array
         storedNotes.forEach(note => {
-            removeItemFromDom(note[0]); // rimozione dell'elemento dalla lista al DOM
-            window.localStorage.removeItem(note[0]); // rimozione della nota dal local storage
+            removeItemFromDom(note[0]);
+            window.localStorage.removeItem(note[0]);
         });
-        storedNotes = []; // svuoto la memoria del javascript eliminando tutte le note dall'array
+        storedNotes = [];
     });
 
 
 });
 
-// aggiunta singolo item al DOM
+
 function addItemToDom(text, list, noteId) {
     const el = document.createElement('li');
     const textNode = document.createTextNode(text);
-    el.setAttribute('id', noteId); // aggiungo un id alla nota al DOM (la stessa che ho salvato al local storage ed in memoria)
+    el.setAttribute('id', noteId);
     el.appendChild(textNode);
     el.classList.add('notepad__item');
     list.appendChild(el);
